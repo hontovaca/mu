93ff68c346b34382012d25a699b8a10408fab429
Muh Muhten <muh.muhten@gmail.com>
2015-11-14 16:09:02 -0500

    give the user access to its supervisor controls

--- over/etc/rc/avail/user2.lua
+++ over/etc/rc/avail/user2.lua
@@ -8,11 +8,21 @@ for ent in _G.io.lines "/etc/passwd" do
       run = realign([[
       #!/usr/bin/execlineb -P
       fdclose 0 fdmove -c 2 1
-      cd %s
-      s6-setuidgid %s
-      tryexec -c { %s/.user2-run }
-      s6-pause
-      ]]):format(home, user, home),
+      define USER %q
+      export HOME %q import -i HOME
+      getcwd SERVICE import -i SERVICE
+      getpid USERPID import -i USERPID
+
+      s6-envuidgid $USER
+      if { import -i GID chgrp $GID supervise supervise/control }
+      if { chmod g+x supervise }
+      if { chmod g+w supervise/control }
+      s6-applyuidgid -U
+
+      cd $HOME
+      tryexec -c { ./.user2-run }
+      s6-svc -O $SERVICE
+      ]]):format(user, home),
     }
 
     rc["user2-log:" .. user]:merge {
@@ -20,9 +30,9 @@ for ent in _G.io.lines "/etc/passwd" do
       consumer_for = "user2:" .. user,
       run = realign([[
       #!/usr/bin/execlineb -P
-      fdclose 1 fdclose 2
-      s6-setuidgid %s
-      s6-log t %s/.user2-uncaught
+      fdclose 0 fdclose 1 fdclose 2
+      s6-setuidgid %q
+      s6-log t %q/.user2-uncaught
       ]]):format(user, home),
     }
 
